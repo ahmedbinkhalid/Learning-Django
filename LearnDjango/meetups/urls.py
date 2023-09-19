bf3f3017393c7f0a0a8972401c8ed34e3a31d3c9
Ahmed Bin Khalid <101318581+ahmedbinkhalid@users.noreply.github.com>
2023-09-19 22:59:32 +0500

    Serving and Displaying Images
    Serving and Displaying Images to users

--- LearnDjango/meetups/urls.py
+++ LearnDjango/meetups/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns =[
     
     path('meetups/', views.index, name='all-meetups'), # our-domain.com/meetups
     path('meetups/<slug:meetup_slug>', views.meetup_details, name='meetup-detail'),# our-domain.com/meetups/ <dynamic-path-segment>
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
