d153295aaa81acf602fa5ce6eba6c321d63bd72a
Ahmed Bin Khalid <101318581+ahmedbinkhalid@users.noreply.github.com>
2023-09-19 19:21:52 +0500

    Learned include
    learned include to make the code re-useable and also learned how to handle to manipulation with include

--- LearnDjango/env/Scripts/activate.bat
+++ LearnDjango/env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\AHMED\Desktop\env
+set VIRTUAL_ENV=E:\Learning-Django\LearnDjango\env
 
 if not defined PROMPT set PROMPT=$P$G
 
