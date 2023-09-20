02d68ac7547c47318d5d29b7260692b68f95efb5
Ahmed Bin Khalid <101318581+ahmedbinkhalid@users.noreply.github.com>
2023-09-20 16:40:25 +0500

    Setting One-to-many Relationships

--- LearnDjango/meetups/admin.py
+++ LearnDjango/meetups/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from .models import Meetup
+from.models import Location
 
 # Register your models here.
 
 class MeetupAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug')
-    list_filter = ('title',)
+    list_filter = ('Location',)
     prepopulated_fields = {'slug': ('title',)}
 admin.site.register(Meetup, MeetupAdmin)
+admin.site.register(Location)
