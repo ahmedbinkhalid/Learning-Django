1a6d6ed559bd0b52aaaa9aa64eab87fac415cda0
Ahmed Bin Khalid <101318581+ahmedbinkhalid@users.noreply.github.com>
2023-09-20 22:29:00 +0500

    Setting many-to-many Relations

--- LearnDjango/meetups/admin.py
+++ LearnDjango/meetups/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Meetup
-from.models import Location
+from .models import Location
+from .models import Participant
 
 # Register your models here.
 
@@ -10,3 +11,4 @@ class MeetupAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
 admin.site.register(Meetup, MeetupAdmin)
 admin.site.register(Location)
+admin.site.register(Participant)
--- LearnDjango/meetups/models.py
+++ LearnDjango/meetups/models.py
@@ -6,13 +6,20 @@ class Location(models.Model):
    name = models.CharField(max_length=200)
    address = models.CharField(max_length=300)
    def __str__(self):
-      return f'{self.name} - {self.address}'  
+      return f'{self.name} - {self.address}' 
+
+class Participant(models.Model):
+   email = models.EmailField(unique=True)
+   def __str__(self):
+      return self.email
+    
 class Meetup(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
     description = models.TextField()
     image = models.ImageField(upload_to = 'images')
     Location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    Participants = models.ManyToManyField(Participant, blank=True, null=True)
 
     def __str__(self):
      return f'{self.title} - {self.slug}'
