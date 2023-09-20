d0947a02f2dc83bf2725c857b9f1dff77d4dd7d6
Ahmed Bin Khalid <101318581+ahmedbinkhalid@users.noreply.github.com>
2023-09-20 22:55:28 +0500

    More Meetup Fields and outputting related data

--- LearnDjango/meetups/admin.py
+++ LearnDjango/meetups/admin.py
@@ -6,8 +6,8 @@ from .models import Participant
 # Register your models here.
 
 class MeetupAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug')
-    list_filter = ('Location',)
+    list_display = ('title', 'date')
+    list_filter = ('Location','date')
     prepopulated_fields = {'slug': ('title',)}
 admin.site.register(Meetup, MeetupAdmin)
 admin.site.register(Location)
