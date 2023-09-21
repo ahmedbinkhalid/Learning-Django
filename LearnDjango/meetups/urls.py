368089e1b2ef673904ff351880b3115be7ca92f7
Ahmed Bin Khalid <101318581+ahmedbinkhalid@users.noreply.github.com>
2023-09-21 22:33:15 +0500

    Optimizing URLs

--- LearnDjango/meetups/urls.py
+++ LearnDjango/meetups/urls.py
@@ -6,7 +6,7 @@ from django.conf import settings
 
 urlpatterns =[
     
-    path('meetups/', views.index, name='all-meetups'), # our-domain.com/meetups
-    path('meetups/<slug:meetup_slug>success', views.confirm_registration, name = 'confirm-registration'),
-    path('meetups/<slug:meetup_slug>', views.meetup_details, name='meetup-detail'),# our-domain.com/meetups/ <dynamic-path-segment>
+    path('', views.index, name='all-meetups'), # our-domain.com/meetups
+    path('<slug:meetup_slug>success', views.confirm_registration, name = 'confirm-registration'),
+    path('<slug:meetup_slug>', views.meetup_details, name='meetup-detail'),# our-domain.com/meetups/ <dynamic-path-segment>
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
