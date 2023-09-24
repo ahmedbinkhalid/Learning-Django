f67386ea4acab8285bb4357254a2e96db1243aa6
Ahmed Bin Khalid <101318581+ahmedbinkhalid@users.noreply.github.com>
2023-09-24 16:51:06 +0500

    Django-Admin set field to display
    Learned two ways to set Django-Admin fields to display

--- club/members/admin.py
+++ club/members/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 from . models import Members
 
 # Register your models here.
-admin.site.register(Members)
+class MembersAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'date')
+admin.site.register(Members, MembersAdmin)
--- club/members/models.py
+++ club/members/models.py
@@ -7,3 +7,5 @@ class Members(models.Model):
     last_name = models.CharField(max_length=15)
     phone = models.IntegerField(null=True)
     date = models.DateField(null=True)
+    # def __str__(self):
+    #   return f'{self.first_name} {self.last_name}'
