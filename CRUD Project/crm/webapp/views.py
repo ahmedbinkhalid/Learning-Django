de68df90ecb37ce82016ea9c6369a691fe3207b6
Ahmed Bin Khalid <101318581+ahmedbinkhalid@users.noreply.github.com>
2023-10-17 23:48:15 +0500

    register page setup

--- CRUD Project/crm/webapp/views.py
+++ CRUD Project/crm/webapp/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .forms import UserCreationForm
 
-# Create your views here.
+# Homepage
 
 def home(request):
     return render(request, 'webapp/index.html')
+
+# Register a User
+
+def register(request):
+
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # return redirect()
+    context = {'form' : form}
+
+    return render(request, 'webapp/register.html', context=context)
