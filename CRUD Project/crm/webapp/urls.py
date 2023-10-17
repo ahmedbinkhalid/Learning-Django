24dfe23e309ee98d47c946aa87ab19136015d1dd
Ahmed Bin Khalid <101318581+ahmedbinkhalid@users.noreply.github.com>
2023-10-18 00:25:42 +0500

    User authentication complete
    Register, Login, Logout complete

--- CRUD Project/crm/webapp/urls.py
+++ CRUD Project/crm/webapp/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.home, name=""),
 
     path('register', views.register, name="register"),
+    path('my-login', views.my_login, name="my-login" )
 ]
--- CRUD Project/crm/webapp/views.py
+++ CRUD Project/crm/webapp/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .forms import UserCreationForm
+from .forms import UserCreationForm, LoginForm
+from django.contrib.auth.models import auth
+from django.contrib. auth import authenticate
+
+
 
 # Homepage
 
@@ -17,7 +21,39 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            # return redirect()
+            return redirect("my-login")
     context = {'form' : form}
 
     return render(request, 'webapp/register.html', context=context)
+
+# Login a User
+
+def my_login(request):
+
+    form = LoginForm()
+    if request.method == 'POST':
+        form = LoginForm(request, data = request.POST)
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username= username, password= password)
+            if user is not None:
+                auth.login(request, user)
+
+                # return redirect("")
+
+    context = {'form': form}
+    return render(request, 'webapp/my-login.html', context=context)
+
+# Logout a User
+
+def user_logout(request):
+
+    auth.logout(request)
+
+    return redirect("my-login")
+
+
+
+
