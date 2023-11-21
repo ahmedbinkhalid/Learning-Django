734bf020cb87ae00d42a8e99cd53dee79b908439
Ahmed Bin Khalid <101318581+ahmedbinkhalid@users.noreply.github.com>
2023-11-21 17:28:08 +0500

    defining the models in the candidate and hr

--- 30_Days_of_Django/Project Job Posting/jobforme/candidate/models.py
+++ 30_Days_of_Django/Project Job Posting/jobforme/candidate/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class MyJobList(models.Model):
+    user = models.OneToOneField(to=User,on_delete= models.CASCADE)
+    job =models.ForeignKey(to= CandidateApplication, on_delete=models.CASCADE)
+    applied_date = models.DateTimeField(auto_now_add=True)
+
+class isSortList(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    job = models.OneToOneField(to= JobPost, on_delete=models.CASCADE)
+    applied_date = models.DateTimeField(auto_now_add=True)
--- 30_Days_of_Django/Project Job Posting/jobforme/hr/models.py
+++ 30_Days_of_Django/Project Job Posting/jobforme/hr/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Hr(models.Model):
+    user = models.OneToOneField(to=User, on_delete=models.CASCADE)
+
+class JobPost(models.Model):
+    user = models.ForeignKey(to= User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    address= models.CharField(max_length=200)
+    companyName = models.CharField(max_length=200)
+    salaryLow = models.IntegerField(default=0)
+    salaryHigh = models.IntegerField(default=0)
+    applycount = models.IntegerField(default=0)
+    lastdatetoapply = models.DateField()
+
+    def __str__(self):
+        return str(self.title)
+
+STATUS_CHOICE = ((
+    'panding', 'panding'
+    'Selected', 'Selected'
+))
+class CandidateApplication(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    job = models.OneToOneField(to=JobPost, on_delete=models.CASCADE)
+    passingyear = models.IntegerField()
+    yearofExp = models.IntegerField(default=0)
+    resume = models.FileField(upload_to='resume')
+    status = models.CharField(choices=STATUS_CHOICE, max_length=20, default='Panding')
+
+class SelectedCandidateJob(models.Model):
+    job = models.ForeignKey(to=JobPost, on_delete=models.CASCADE)
+    candidate = models.OneToOneField(to=CandidateApplication, on_delete=models.CASCADE)
